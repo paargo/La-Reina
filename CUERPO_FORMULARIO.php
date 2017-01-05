814c151172d4659776c6338aecc5fd8ee8528fc0
paargo <gpabloariel@gmail.com>
2017-01-05 13:52:40 -0300

    Correccion de parseo

--- CUERPO_FORMULARIO.php
+++ CUERPO_FORMULARIO.php
@@ -42,7 +42,7 @@
 							/*$_POST['ID_FORM']*/
 							$resultado=mysqli_query($result_coneccion,$query);
 							while ($form = mysqli_fetch_assoc($resultado)) {
-							echo '<input type = "'.$form["TIPO"].'" name="'.$form["NOMBRE_CAMPO"].">"; 
+							echo '<input type = "'.$form["TIPO"].'" name="'."$form["NOMBRE_CAMPO"].'>'"; 
 							echo '<br> entra 1 <br>';
 							}
 						?>				
