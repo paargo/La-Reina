6c5b893bde2d6f81439c5ef25a97501e488f6739
paargo <gpabloariel@gmail.com>
2017-01-08 13:52:49 -0300

    HABILITACION DE DIVERSOS FORMULARIOS
    HABILITACION DE DIVERSOS FORMULARIOS

--- SELECCION_FORMULARIO.php
+++ SELECCION_FORMULARIO.php
@@ -49,6 +49,7 @@
 							}				
 					?>
 				</select>
+				<input type ="hidden" name = "ID_FORM" value = <?php echo $form["ID_FORM"]; ?>> 
 				<input type ="hidden" name = "Usuario" value = <?php echo $usuario['USUARIO']; ?>> 
 				<br><br>
 				<input type="submit" value="Ingresar">
