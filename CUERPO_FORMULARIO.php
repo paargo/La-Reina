0758df36dc08081d196a85476018d0f630acc1d2
paargo <gpabloariel@gmail.com>
2017-01-08 14:16:49 -0300

    Habilitacion definitiva de distintos forms

--- CUERPO_FORMULARIO.php
+++ CUERPO_FORMULARIO.php
@@ -36,8 +36,7 @@
 		<tr><td></td>
 			<td>
 				<?php 	
-					echo $_POST['FORMULARIO'];
-					$query_cab="SELECT NOMBRE,ACCION FROM u845291486_reina.CABFORM WHERE ID_FORM=1";
+					$query_cab="SELECT NOMBRE,ACCION FROM u845291486_reina.CABFORM WHERE ID_FORM=".$_POST['FORMULARIO'];
 					$resultado_cab=mysqli_query($result_coneccion,$query_cab);
 					$form_cab = mysqli_fetch_array($resultado_cab, MYSQLI_ASSOC);	
 				?>		
