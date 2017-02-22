895f59cdda02fdff3b5c99e8c6affbf7dfd49be5
paargo <gpabloariel@gmail.com>
2017-02-22 14:05:27 -0300

    qry ver

--- CUERPO_FORMULARIO.php
+++ CUERPO_FORMULARIO.php
@@ -100,7 +100,7 @@
 
 				
 									$qry_res_fin=mysqli_query($result_coneccion,$qry_fin);
-									echo $qry_fin;
+									echo $qry_res_fin;
 									while ($seleccion = mysqli_fetch_assoc($qry_res_fin)) {
 									
 										$valor = $qry_res_fin["1"];
